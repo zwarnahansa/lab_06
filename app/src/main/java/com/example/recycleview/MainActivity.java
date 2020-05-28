0232bfb90d6458091ea730660b01584adf9fb050
Zwarnahansa <it18211474@my.sliit.lk>
2020-05-29 01:48:48 +0530

    push1

---
+++ app/src/main/java/com/example/recycleview/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.recycleview;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: started");
+        initImageBitmaps();
+    }
+
+    private void initImageBitmaps() {
+        Log.d(TAG, "initImageBitmaps: started");
+        mImageUrls.add("https://assets.traveltriangle.com/blog/wp-content/uploads/2018/11/About-Sigiriya.jpg");
+        mNames.add("Seegiriya");
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2015/06/Demodara-Nine-Arch-Bridge.jpg");
+        mNames.add("Ella");
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2015/06/Train-ride-from-Kandy-to-Nuwara-Eliya.jpg");
+        mNames.add("Nuwara Eliya");
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2015/06/Pinnawala-Elephant-Orphanage.jpg");
+        mNames.add("Pinnawala Elephant Orphanage");
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2015/06/Ruins-of-Polonnaruwa.jpg");
+        mNames.add("Polonnaruwa");
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2015/06/Adams-Peak.jpg");
+        mNames.add("Adams Peak");
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2015/06/Mirissa-Fisheries-Harbor.jpg");
+        mNames.add("Mirissa");
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2015/06/Leopards.jpg");
+        mNames.add("Yala National Park");
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2015/06/Colombo.jpg");
+        mNames.add("Colombo");
+        mImageUrls.add("https://img.traveltriangle.com/blog/wp-content/tr:w-700,h-400/uploads/2015/06/Jaffna.jpg");
+        mNames.add("Jaffna");
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+        Log.d(TAG, "initRecyclerView: started");
+        RecyclerView recycleview = findViewById(R.id.recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(mNames,mImageUrls,this);
+        recycleview.setAdapter(adapter);
+        recycleview.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+}
